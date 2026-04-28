06ee20b9f53ac4eb47eb4c95d3458175e66033af
HC-VP <hugo.cemeli@gmail.com>
2026-04-28 11:42:18 +0200

    User model created with secure password hashing using bcrypt

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Import bcrypt to Hash passwords
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'le nom est requis'],
+      trim: true
+    },    
+    email: {
+      type: String,
+      required: [true, 'un email est requis'],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true      
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+//Hash password when modified
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+    
+});
+
+// Check if password ok
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
