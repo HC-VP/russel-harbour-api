6dc777cb3e1e921a3bb1d51c2bfa31d798b51d3a
HC-VP <hugo.cemeli@gmail.com>
2026-04-28 11:51:04 +0200

    Catway and Reservation models created

---
+++ models/Reservation.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const reservationSchema = new mongoose.Schema(
+  {
+    catwayNumber: {
+      type: Number,
+      required: true
+    },
+    clientName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    boatName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    checkIn: {
+      type: Date,
+      required: true
+    },
+    checkOut: {
+      type: Date,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Reservation", reservationSchema);
