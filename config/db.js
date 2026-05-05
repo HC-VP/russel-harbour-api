c614e9a9fcab0143881f59f0f99fef765ebfc952
HC-VP <hugo.cemeli@gmail.com>
2026-05-05 12:33:50 +0200

    finalize railway deployment config

--- config/db.js
+++ config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-    console.log("ENV KEYS:", Object.keys(process.env).filter(key => key.includes("MONGO")));
+  try {  
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
