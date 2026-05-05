cafa170ca2b328a8196acf27c84713d389dd275b
HC-VP <hugo.cemeli@gmail.com>
2026-05-05 12:13:03 +0200

    debug: inspect railway env variables

--- config/db.js
+++ config/db.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+    console.log("ENV KEYS:", Object.keys(process.env).filter(key => key.includes("MONGO")));
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
